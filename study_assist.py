7b376bbf51a177633e0dde815bfecca8c52ac3f3
Tensor <narutokelechi@gmail.com>
2024-06-28 00:02:33 +0100

    functional chat and google embeddding, fix file upload

--- study_assist.py
+++ study_assist.py
@@ -1,18 +1,16 @@
 import os
-from typing_extensions import Doc
+from posix import write
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import FAISS
-from langchain.vectorstores import DocArrayInMemorySearch
+from langchain_community.vectorstores import faiss
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.docstore.document import Document
 from langchain.memory import ConversationBufferMemory
 from langchain_core.output_parsers import StrOutputParser
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from tempfile import NamedTemporaryFile
 from dotenv import load_dotenv
-from tqdm.auto import tqdm
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
+from pandas.io.formats.style import doc
 import streamlit as st
 
 # import asyncio
@@ -39,12 +37,18 @@ def initialize_resources():
     return llm_gemini
 
 
-@st.cache_resource
 def get_retriever(pdf_file):
-    pdf_loader = PyPDFLoader(pdf_file, extract_images=False)
-    pages = pdf_loader.load()
+    # path = os.path.join(os.getcwd(), pdf_file.name)
 
-    underlying_embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    with NamedTemporaryFile(suffix="pdf") as temp:
+        temp.write(pdf_file.getvalue())
+        pdf_loader = PyPDFLoader(temp.name, extract_images=False)
+        pages = pdf_loader.load()
+
+    st.write(f"AI Chatbot for {course_material}")
+
+    # underlying_embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    underlying_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=20,
@@ -53,9 +57,7 @@ def get_retriever(pdf_file):
         separators="\n",
     )
     documents = text_splitter.split_documents(pages)
-    vectorstore = DocArrayInMemorySearch.from_documents(
-        documents, underlying_embeddings
-    )
+    vectorstore = faiss.FAISS.from_documents(documents, underlying_embeddings)
     doc_retiever = vectorstore.as_retriever(
         search_type="mmr", search_kwargs={"k": 5, "fetch_k": 10}
     )
@@ -65,31 +67,13 @@ def get_retriever(pdf_file):
 
 chat_model = initialize_resources()
 
-
-def query_response(query, retriever):
-    prompt = ChatPromptTemplate.from_template(
-        """Answer the question based only on the following context:
-            {context}
-            Question: {question}
-        """
-    )
-
-    chain = (
-        {"context": retriever, "question": RunnablePassthrough()}
-        | prompt
-        | chat_model
-        | StrOutputParser()
-    )
-    response = chain.invoke(query)
-
-    return response
-
-
 # Streamlit UI
 # Course list and pdf retrieval
 
 courses = ["PMB", "PCL", "Kelechi_research"]  # "GSP", "CPM", "PCG",  "PCH"
 course_pdfs = None
+doc_retriever = None
+conversational_chain = None
 
 course = st.sidebar.selectbox("Choose course", (courses))
 docs_path = f"pdfs/{course}"
@@ -98,42 +82,115 @@ pdfs = [os.path.join(docs_path, pdf) for pdf in course_pdfs]
 
 course_material = "{Not selected}"
 
-try:
-    # if st.sidebar.button('Get available course pdfs'):
-    if course_pdfs:
-        course_material = st.sidebar.selectbox(
-            "Select course pdf", (pdf for pdf in pdfs)
-        )
-    if course_material:
-        st.write(f"AI Chatbot for **{course}**: {course_material}")
-        st.success("File loading successful, vector db initialized")
 
-    else:
-        uploaded_file = st.sidebar.file_uploader("or Upload your own pdf", type="pdf")
-        if uploaded_file is not None:
-            course_material = uploaded_file
-            st.write(f"AI Chatbot for **{course}**: {uploaded_file.filename}")
+@st.cache_resource
+def query_response(query, _retriever):
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversational_chain = ConversationalRetrievalChain.from_llm(
+        llm=chat_model, retriever=_retriever, memory=memory, verbose=False
+    )
+    response = conversational_chain.run(query)
 
-            retriever = get_retriever(course_material)
-
-        st.success("File loading successful, vector db initialized")
-
-except Exception as e:
-    st.error(e)
-
-doc_retriever = None
-conversational_chain = None
-
-if st.sidebar.button("Load pdf"):
-    with st.spinner("Loading material..."):
-        doc_retriever = load_pdf(course_material)
-        conversational_chain = ConversationalRetrievalChain.from_llm(
-            chat_model, doc_retriever
-        )
-
-conversational_chain = ConversationalRetrievalChain.from_llm(chat_model, doc_retriever)
+    return response
 
 
+def get_file():
+    # if course_pdfs:
+    # course_material = st.sidebar.selectbox(
+    #   "Select course pdf", (pdf for pdf in pdfs), key="pdf"
+    # )
+    course_material = st.file_uploader("or Upload your own pdf", type="pdf", key="doc")
+    with NamedTemporaryFile(suffix="pdf") as temp:
+        temp.write(course_material.getvalue())
+
+    st.write(f"AI Chatbot for {course_material}")
+
+    print(course_material.getvalue())
+    return course_material.name
+
+
+# try:
+# if st.sidebar.button('Get available course pdfs'):
+# if course_pdfs:
+#   course_material = st.sidebar.selectbox("Select course pdf", (pdf for pdf in pdfs))
+
+# if course_material:
+# st.write(f"AI Chatbot for **{course}**: {course_material}")
+# st.success("File loading successful, vector db initialized")
+
+# else:
+#   uploaded_file = st.sidebar.file_uploader("or Upload your own pdf", type="pdf")
+#  if uploaded_file is not None:
+#     course_material = uploaded_file
+#    st.write(f"AI Chatbot for **{course}**: {uploaded_file.filename}")
+
+# doc_retriever = get_retriever(course_material)
+
+
+# if st.button("Load pdf"):
+#   with st.spinner("Loading material..."):
+#      # doc_retriever = load_pdf(course_material)
+#     doc_retriever = get_retriever(course_material)
+#
+#       st.success("File loading successful, vector db initialized")
+#      input_text = st.text_input(label="Ask...")
+
+# response = conversational_chain.invoke(input)
+
+if "doc" not in st.session_state:
+    st.session_state.doc = ""
+
+
+course_material = st.file_uploader("or Upload your own pdf", type="pdf")
+
+if st.session_state != "":
+    # urls = get_page_urls(base_url)
+    doc_retriever = get_retriever(course_material)
+    st.success("File loading successful, vector db initialize")
+
+    # We store the conversation in the session state.
+    # This will be use to render the chat conversation.
+    # We initialize it with the first message we want to be greeted with.
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "assistant", "content": "How may I help you today?"}
+        ]
+
+    if "current_response" not in st.session_state:
+        st.session_state.current_response = ""
+
+    # We loop through each message in the session state and render it as
+    # a chat message.
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # We initialize the quantized LLM from a local path.
+    # Currently most parameters are fixed but we can make them
+    # configurable.
+    # llm_chain = create_chain(retriever)
+
+    # We take questions/instructions from the chat input to pass to the LLM
+    if user_prompt := st.chat_input("Your message here", key="user_input"):
+        # Add our input to the session state
+        st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+        # Add our input to the chat window
+        with st.chat_message("user"):
+            st.markdown(user_prompt)
+
+        # Pass our input to the llm chain and capture the final responses.
+        # It is worth noting that the Stream Handler is already receiving the
+        # streaming response as the llm is generating. We get our response
+        # here once the llm has finished generating the complete response.
+        response = query_response(user_prompt, doc_retriever)
+        # Add the response to the session state
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+        # Add the response to the chat window
+        with st.chat_message("assistant"):
+            st.markdown(response)
+#
 st.write("")
 st.write("")
 
